97bae2bfa4df0d077707fe80f61ff76c411055b6
hot666666 <hotcs6@naver.com>
2024-09-11 15:43:59 +0900

    Widget - add SwiftDataWidget in WidgetBundle

---
+++ LearnWidgetKitWidget/LearnWidgetKitWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  LearnWidgetKitWidgetBundle.swift
+//  LearnWidgetKitWidget
+//
+//  Created by hs on 9/11/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct LearnWidgetKitWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        SwiftDataWidget()
+    }
+}
